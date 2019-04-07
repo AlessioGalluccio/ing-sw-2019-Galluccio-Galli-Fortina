c9e0ec263246faf2985e5096fa783a23b3f58b2f
dade145 <davide11.galli@polimi.it>
2019-04-07 16:53:09 +0200

    Added attribute optional

--- src/main/java/it/polimi/se2019/view/ViewControllerMess/StringAndMessage.java
+++ src/main/java/it/polimi/se2019/view/ViewControllerMess/StringAndMessage.java
@@ -3,12 +3,17 @@ package it.polimi.se2019.view.ViewControllerMess;
 public class StringAndMessage {
     private ViewControllerMessage message;
     private String string;
+    private boolean optional;
 
     public String getString() {
         return string;
     }
 
     public ViewControllerMessage getMessage() {
+
+    }
+
+    public boolean isOptional() {
 
     }
 }
