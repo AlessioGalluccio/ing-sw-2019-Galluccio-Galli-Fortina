9548bc7ecc7dd06d978d480fb7f977325d7810df
AlessioGalluccio <alessio.galluccio@mail.polimi.it>
2019-04-15 23:49:54 +0200

    Populated methods of Controller

--- src/main/java/it/polimi/se2019/controller/Controller.java
+++ src/main/java/it/polimi/se2019/controller/Controller.java
@@ -2,11 +2,10 @@ package it.polimi.se2019.controller;
 
 import it.polimi.se2019.model.deck.PowerupCard;
 import it.polimi.se2019.model.handler.GameHandler;
+import it.polimi.se2019.model.handler.Identificator;
 import it.polimi.se2019.model.player.Action;
 import it.polimi.se2019.view.PlayerView;
-import it.polimi.se2019.view.ViewControllerMess.ActionMessage;
-import it.polimi.se2019.view.ViewControllerMess.StringAndMessage;
-import it.polimi.se2019.view.ViewControllerMess.ViewControllerMessage;
+import it.polimi.se2019.view.ViewControllerMess.*;
 
 import java.util.ArrayList;
 import java.util.Observable;
@@ -16,14 +15,101 @@ public class Controller implements Observer {
 
     private ArrayList<ViewControllerMessage> messageListReceived;
     private ArrayList<StringAndMessage> messageListExpected;
+    private int indexExpected = 0;
     private GameHandler gameHandler;
 
-    public Controller() {
+    public Controller(GameHandler gameHandler) {
+        this.gameHandler = gameHandler;
+        this.messageListReceived = new ArrayList<ViewControllerMessage>();
+        this.messageListExpected = new ArrayList<StringAndMessage>();
     }
 
     @Override
     public void update(Observable o, Object arg) {
 
+    }
+
+    @Override
+    public void update(Observable o, PlayerViewMessage arg) {
+        //TODO
+
+        //new Message
+        if(messageListExpected.isEmpty() && arg.getMessageID() == Identificator.ACTION_MESSAGE) {
+            messageListReceived.add(arg);
+            messageListExpected = (gameHandler.getActionByID(arg.getMessageID()).getStringAndMessageExpected());
+        }
+
+        //middle cases
+        else if(arg.getMessageID() == messageListExpected.get(indexExpected).getMessage().getMessageID()) {
+            if(arg.getMessageID() == Identificator.FIRE_MODE_MESSAGE) {
+                //TODO possibleTargets da spedire alla View
+            }
+            if(arg.getMessageID() == Identificator.NEWTON_MESSAGE) {
+                //TODO possibleTargets da spedire alla View
+            }
+            if(arg.getMessageID() == Identificator.TAGBACK_GRANADE_MESSAGE) {
+                //TODO possibleTargets da spedire alla View
+            }
+
+            messageListReceived.add(arg);
+            indexExpected++;
+        }
+
+        //NOPE message
+        else if(arg.getMessageID() == Identificator.NOPE_MESSAGE) {
+            if (messageListExpected.get(indexExpected).isOptional()) {
+                indexExpected++;
+            }
+            else {
+                //TODO messaggio da stampare alla view
+                arg.getAuthorView().printFromController("Warning, this action is required");
+            }
+        }
+
+        //UNDO message
+        //else if(arg.getMessageID() == Identificator.UNDO_MESSAGE) {
+            //TODO da vedere se ha senso di esistere!
+            //indexExpected--;
+            //messageListReceived.remove(indexExpected);
+        //}
+
+        //End of series of messages
+        else if(messageListExpected.size() == indexExpected) {
+            //TODO inserire modifyModel
+            //modifyModel();
+
+            flushMessages();
+        }
+
+        //Message is wrong, an error massage is printed in the view of the player
+        else {
+            arg.getAuthorView().printFromController(messageListExpected.get(indexExpected).getString());
+        }
+
+    }
+
+    /**
+     * command the model to send to the view the possible targets of this firemode
+     * @param fireModeMess the message of this firemode
+     */
+    private void askPossibleTargets(FireModeMessage fireModeMess) {
+        //TODO
+    }
+
+    /**
+     * command the model to send to the view the possible targets of this tagbackGranate card
+     * @param tagbackGranateMess the message of this tagbackGranade card
+     */
+    private void askPossibleTargets(TagbackGranateMessage tagbackGranateMess) {
+        //TODO
+    }
+
+    /**
+     * command the model to send to the view the possible targets of this Newton card
+     * @param newtonMess the message of this Newton card
+     */
+    private void askPossibleTargets(NewtonMessage newtonMess) {
+        //TODO
     }
 
     /**
@@ -44,14 +130,16 @@ public class Controller implements Observer {
      * it empties the messages after Model is modified
      */
     private void flushMessages() {
-
+        messageListReceived = new ArrayList<ViewControllerMessage>();
+        this.messageListExpected = new ArrayList<StringAndMessage>();
+        indexExpected = 0;
     }
 
     /**
      * it empties only the messages of the optional firemode
      */
     private void flushOptionalMessages() {
-
+        //TODO
     }
 
 
