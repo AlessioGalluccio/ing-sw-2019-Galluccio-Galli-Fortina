a595bdb72b2b1e31cecba3d1715a633d221a031a
dade145 <davide11.galli@mail.polimi.it>
2019-05-01 10:56:40 +0200

    Change Client interface

--- src/main/java/it/polimi/se2019/network/Client.java
+++ src/main/java/it/polimi/se2019/network/Client.java
@@ -1,10 +1,12 @@
 package it.polimi.se2019.network;
 
+import it.polimi.se2019.view.ViewControllerMess.ViewControllerMessage;
+
 public interface Client {
 
     /**
      * Send user's message to the Server
      * @param message message to send
      */
-    void send(String message);
+    void send(ViewControllerMessage message);
 }
