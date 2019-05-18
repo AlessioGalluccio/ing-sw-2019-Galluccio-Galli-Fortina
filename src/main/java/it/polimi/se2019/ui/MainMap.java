4a60c75270f7dc3796ac771f1f23fe8c4aeae315
valeriafortina <valeriamaria.fortina@mail.polimi.it>
2019-05-18 17:27:33 +0200

    changed map1.fxml

--- src/main/java/it/polimi/se2019/ui/MainMap.java
+++ src/main/java/it/polimi/se2019/ui/MainMap.java
@@ -7,19 +7,21 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-public class Main extends Application {
+public class MainMap extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("Map1.fxml"));
-        primaryStage.setTitle("MapOne");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.setTitle("Map1");
+        primaryStage.setScene(new Scene(root, 700, 700));
         primaryStage.show();
     }
 
 
     public static void main(String[] args) {
         launch(args);
+
+
     }
 }
 
