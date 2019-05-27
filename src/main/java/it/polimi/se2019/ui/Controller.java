db76ae9150bf8c343fa672f8a53f2bcd199160d5
valeriafortina <valeriamaria.fortina@mail.polimi.it>
2019-05-27 12:43:47 +0200

    added button to show user choises, added methods to choose map and show the selected map

--- src/main/java/it/polimi/se2019/ui/Controller.java
+++ src/main/java/it/polimi/se2019/ui/Controller.java
@@ -5,6 +5,7 @@ package it.polimi.se2019.ui;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
@@ -12,10 +13,12 @@ import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.stage.Stage;
 
+
+
 public class Controller {
     @FXML
     public Label mylabel;
-    @FXML
+
     public ImageView mappa;
     @FXML
     public Button RedWeapon;
@@ -29,6 +32,12 @@ public class Controller {
     public RadioButton rbmap3;
     @FXML
     public RadioButton rbmap4;
+    public Button showMap;
+
+    public static int choosenMap;
+    public Button weaponDeck;
+    public Button powerupDeck;
+
 
     //ogni label o textfield ecc che vado a creare nel file fxml lo devo riportare come attributo nel controller
     //con lo stesso come dato in fx:id e in onAction metto il metodo
@@ -65,23 +74,30 @@ public class Controller {
     }
 
     public void chooseMap(ActionEvent event) throws Exception{
-        //mylabel.setText("ciao");
-        Parent root = FXMLLoader.load(getClass().getClassLoader().getResource("Map1.fxml"));
-        Stage primaryStage = new Stage();
-        primaryStage.setTitle("Mappa1");
-        primaryStage.setScene(new Scene(root, 800, 600));
-        primaryStage.show();
+
 
         //mappa.setImage(new Image("mappa.jpg"));
 
+        if(rbmap1.isSelected()){
 
-        /*
-
-        if(rbmap2.isSelected()){
-            mappa.setImage(new Image("mappa2.jpg"));
+            choosenMap = 1;
+            System.out.println(choosenMap);
         }
 
-        */
+
+        if(rbmap2.isSelected()){
+
+            choosenMap = 2;
+            System.out.println(choosenMap);
+
+
+        }
+
+        Parent root = FXMLLoader.load(getClass().getClassLoader().getResource("Map1.fxml"));
+        Stage primaryStage = new Stage();
+        primaryStage.setTitle("Map1");
+        primaryStage.setScene(new Scene(root, 800, 600));
+        primaryStage.show();
 
 
 
@@ -89,4 +105,16 @@ public class Controller {
     }
 
 
+    public void showMap(ActionEvent event) {
+        System.out.println(choosenMap);
+        if (choosenMap == 2) {
+            mappa.setImage(new Image("mappa2.jpg"));
+        }
+        if (choosenMap == 1) {
+            mappa.setImage(new Image("mappa.jpg"));
+
+        }
+        showMap.setDisable(true);
+        showMap.setVisible(false);
+    }
 }
