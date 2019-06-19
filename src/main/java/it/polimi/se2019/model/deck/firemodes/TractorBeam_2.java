ddb63192509aa3aefccaed7669be9b1c08949536
dade145 <davide11.galli@mail.polimi.it>
2019-06-20 01:17:30 +0200

    Implement method and test

--- src/main/java/it/polimi/se2019/model/deck/firemodes/TractorBeam_2.java
+++ src/main/java/it/polimi/se2019/model/deck/firemodes/TractorBeam_2.java
@@ -1,24 +1,27 @@
 package it.polimi.se2019.model.deck.firemodes;
 
-import it.polimi.se2019.controller.actions.FiremodeOfOnlyMarksException;
-import it.polimi.se2019.controller.actions.Shoot;
 import it.polimi.se2019.controller.actions.WrongInputException;
 import it.polimi.se2019.model.deck.FireMode;
-import it.polimi.se2019.model.deck.Target;
-import it.polimi.se2019.model.handler.GameHandler;
+import it.polimi.se2019.model.handler.Identificator;
+import it.polimi.se2019.model.map.Cell;
 import it.polimi.se2019.model.player.*;
-import it.polimi.se2019.view.remoteView.PlayerView;
 import it.polimi.se2019.view.StringAndMessage;
-import it.polimi.se2019.view.ViewControllerMess.ViewControllerMessage;
 
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 
+
 public class TractorBeam_2 extends FireMode {
+    private final String TOO_DISTANT = "The target you chose is too distant from you.\n";
+    private final String SELECT_PLAYER = "Select a player up to two cells distant from you, even if you can't see it. ";
 
     @Override
     public List<StringAndMessage> getMessageListExpected() {
-        return null;
+        StringAndMessage firstMessage = new StringAndMessage(Identificator.PLAYER_MESSAGE, SELECT_PLAYER);
+        List<StringAndMessage> list = new ArrayList<>();
+        list.add(firstMessage);
+        return list;
     }
 
     @Override
@@ -35,32 +38,34 @@ public class TractorBeam_2 extends FireMode {
 
     @Override
     public void fire() throws WrongInputException{
+        if(shoot.getTargetsPlayer().size() != 1){
+            throw new WrongInputException(CANT_DO_FIRE);
+        }
+        for(Player target : shoot.getTargetsPlayer()){
+            target.setPosition(author.getCell());
+            addDamageAndMarks(target,3,0, true);
+        }
+        super.fire();
 
     }
 
-    @Override
-    public void addCell(int x, int y) throws WrongInputException {
+    //void addCell(int x, int y) call the method of the super class
 
-    }
 
     @Override
     public void addPlayerTarget(int playerID) throws WrongInputException {
-
+        Player target = gameHandler.getPlayerByID(playerID);
+        List<Cell> possibleCell = gameHandler.getMap().getCellAtDistance(author.getCell(), 2);
+        List<Player> possibleTarget = new LinkedList<>();
+        for(Cell cell : possibleCell) {
+            possibleTarget.addAll(cell.getPlayerHere());
+        }
+        if(possibleTarget.contains(target)) shoot.addPlayerTargetFromFireMode(target, true);
+        else{
+            throw new WrongInputException(TOO_DISTANT);
+        }
     }
 
-    @Override
-    public void addTargetingScope(int targetingCardID, AmmoBag cost) throws WrongInputException, NotPresentException, NotEnoughAmmoException, FiremodeOfOnlyMarksException {
-
-    }
-
-    @Override
-    public void addOptional(int numOptional) throws WrongInputException, NotEnoughAmmoException {
-
-    }
-
-    @Override
-    public void addNope() throws WrongInputException {
-
-    }
+    // void addOptional(int numOptional) call the method of the super class
 
 }
