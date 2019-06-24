b4a309cb43de057f241e20a4ec05c680dbc466de
AlessioGalluccio <alessio.galluccio@mail.polimi.it>
2019-06-25 01:47:41 +0200

    added ShotGun 1 and 2 and their tests

--- src/main/java/it/polimi/se2019/model/deck/firemodes/ShotGun_2.java
+++ src/main/java/it/polimi/se2019/model/deck/firemodes/ShotGun_2.java
@@ -6,6 +6,8 @@ import it.polimi.se2019.controller.actions.WrongInputException;
 import it.polimi.se2019.model.deck.FireMode;
 import it.polimi.se2019.model.deck.Target;
 import it.polimi.se2019.model.handler.GameHandler;
+import it.polimi.se2019.model.handler.Identificator;
+import it.polimi.se2019.model.map.Cell;
 import it.polimi.se2019.model.player.*;
 import it.polimi.se2019.view.remoteView.PlayerView;
 import it.polimi.se2019.view.StringAndMessage;
@@ -18,9 +20,18 @@ public class ShotGun_2 extends FireMode {
 
     private static final long serialVersionUID = 1155806501696304392L;
 
+    //messages
+    public static final String SELECT_PLAYER_DISTANT_ONE = "Select a target distant one cell. ";
+
+
+    //errors
+    public static final String INVALID_TARGET_SHOTGUN2 = "Invalid target. ";
+
     @Override
     public List<StringAndMessage> getMessageListExpected() {
-        return null;
+        List<StringAndMessage> list = new ArrayList<>();
+        list.add(new StringAndMessage(Identificator.PLAYER_MESSAGE, SELECT_PLAYER_DISTANT_ONE));
+        return list;
     }
 
     @Override
@@ -37,28 +48,25 @@ public class ShotGun_2 extends FireMode {
 
     @Override
     public void fire() throws WrongInputException{
-
+        if(shoot.getTargetsPlayer().isEmpty()){
+            throw new WrongInputException(CANT_DO_FIRE);
+        }
+        else{
+            addDamageAndMarks(shoot.getTargetsPlayer().get(0),2,0,true);
+            super.fire();
+        }
     }
 
-    @Override
-    public void addCell(int x, int y) throws WrongInputException {
-
-    }
 
     @Override
     public void addPlayerTarget(int playerID) throws WrongInputException {
-
+        Player target = gameHandler.getPlayerByID(playerID);
+        if(gameHandler.getMap().getDistance(author.getCell(),target.getCell()) == 1){
+            shoot.addPlayerTargetFromFireMode(target,true);
+        }
+        else{
+            throw new WrongInputException(INVALID_TARGET_SHOTGUN2);
+        }
     }
-
-    @Override
-    public void addTargetingScope(int targetingCardID, AmmoBag cost) throws WrongInputException, NotPresentException, NotEnoughAmmoException, FiremodeOfOnlyMarksException {
-
-    }
-
-    @Override
-    public void addOptional(int numOptional) throws WrongInputException, NotEnoughAmmoException {
-
-    }
-
 
 }
